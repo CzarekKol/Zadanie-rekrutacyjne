bf6ce5e4c016ac98defce57a6815871188315387
Czarek <cezary.kolpiewicz@onet.pl>
2022-12-19 18:08:33 +0100

    commit

---
+++ src/Controller/NewController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewController extends AbstractController
+{
+    /**
+     * @Route("/",name="homepage")
+     */
+    public function homepage(): Response
+    {
+        return new Response('Hi!');
+    }
+
+    /**
+     * @Route("/{sraka}",name="TwigAction")
+     */
+    public function TwigAction(string $sraka)
+    {
+        return $this->render('test.html.twig',[
+            'holyshit' => $sraka
+        ]);
+    }
+}
